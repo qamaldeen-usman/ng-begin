a9c00eee624e361081ab417aa34101cbbc36f293
qamaldeen-usman <qamaldeenusman@gmail.com>
2026-04-15 21:46:21 +0100

    wed-night-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', loadComponent: () => import('../features/homepage/homepage').then((m) => m.Homepage)},
+  {path: 'about', loadComponent: () => import('../features/about/about').then((m) => m.About)},
+  {path: 'services', loadComponent: () => import('../features/services/services').then((m)=> m.Services)},
+  {path: 'orders', loadComponent: () => import('../features/orders/orders').then((m) => m.Orders)},
+  {path: 'customers', loadComponent: () => import('../features/customers/customers').then((m) => m.Customers)},
+];
--- src/core/header/header.ts
+++ src/core/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgbCollaspeModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
