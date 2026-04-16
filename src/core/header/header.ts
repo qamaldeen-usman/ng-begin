c015b38e310c3945b940f20d5e80aaa48068f3d3
qamaldeen-usman <qamaldeenusman@gmail.com>
2026-04-16 08:35:59 +0100

    thurs-day-commit

--- src/core/header/header.ts
+++ src/core/header/header.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
-import {NgbCollaspeModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgbCollapseModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-
+  isMenuOpen = false;
 }
--- src/features/homepage/homepage.ts
+++ src/features/homepage/homepage.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, OnInit,ChangeDetectorRef } from '@angular/core';
 import Typed from 'typed.js';
 
 @Component({
@@ -7,17 +7,62 @@ import Typed from 'typed.js';
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
-export class Homepage implements AfterViewInit {
+// export class Homepage implements AfterViewInit {
+//
+//   @ViewChild('typedElement', { static: true }) typedElement!: ElementRef;
+//
+//   ngAfterViewInit() {
+//     new Typed(this.typedElement.nativeElement, {
+//       strings: ['remake.', 'revamp.', 'beautify.'],
+//       typeSpeed: 60,
+//       backSpeed: 30,
+//       backDelay: 2000,
+//       loop: true
+//     });
+//   }
+// }
 
-  @ViewChild('typedElement', { static: true }) typedElement!: ElementRef;
+export class Homepage implements OnInit {
+  days = 21;
+  hours = 24;
+  minutes = 60;
+  seconds = 60;
 
-  ngAfterViewInit() {
-    new Typed(this.typedElement.nativeElement, {
-      strings: ['remake.', 'revamp.', 'beautify.'],
-      typeSpeed: 60,
-      backSpeed: 30,
-      backDelay: 2000,
-      loop: true
-    });
+  targetDate = new Date('2026-04-16T08:15:05');
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const now = new Date().getTime();
+      const distance = this.targetDate.getTime() - now;
+
+      if (distance < 0) return;
+
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+      this.minutes = Math.floor((distance / (1000 * 60)) % 60);
+      this.seconds = Math.floor((distance / 1000) % 60);
+
+      console.log(this.seconds); // debug
+
+    }, 1000);
   }
+
+  // startCountdown() {
+  //   setInterval(() => {
+  //     const now = new Date().getTime();
+  //     const distance = this.targetDate.getTime() - now;
+  //
+  //     if (distance < 0) return;
+  //
+  //     this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //     this.hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  //     this.minutes = Math.floor((distance / (1000 * 60)) % 60);
+  //     this.seconds = Math.floor((distance / 1000) % 60);
+  //
+  //     this.cd.detectChanges(); // 🔥 force update
+  //
+  //   }, 1000);
+
 }
