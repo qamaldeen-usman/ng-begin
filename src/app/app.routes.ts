581e5355627cdc8e8655468129a311b3915fa770
qamaldeen-usman <qamaldeenusman@gmail.com>
2026-05-11 09:14:02 +0100

    11th apr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', loadComponent: () => import('../features/homepage/homepage').then((m) => m.Homepage)},
+  {path: '', loadComponent: () => import('../features/homepage/homepage').then((m) => m.HomePage)},
   {path: 'about', loadComponent: () => import('../features/about/about').then((m) => m.About)},
   {path: 'services', loadComponent: () => import('../features/services/services').then((m)=> m.Services)},
   {path: 'orders', loadComponent: () => import('../features/orders/orders').then((m) => m.Orders)},
--- src/features/homepage/homepage.ts
+++ src/features/homepage/homepage.ts
@@ -1,68 +1,21 @@
-import { Component, AfterViewInit, ElementRef, ViewChild, OnInit,ChangeDetectorRef } from '@angular/core';
+import { Component, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import Typed from 'typed.js';
+import { jarallax } from 'jarallax';
+
 
 @Component({
   selector: 'app-homepage',
   imports: [],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-// export class Homepage implements AfterViewInit {
-//
-//   @ViewChild('typedElement', { static: true }) typedElement!: ElementRef;
-//
-//   ngAfterViewInit() {
-//     new Typed(this.typedElement.nativeElement, {
-//       strings: ['remake.', 'revamp.', 'beautify.'],
-//       typeSpeed: 60,
-//       backSpeed: 30,
-//       backDelay: 2000,
-//       loop: true
-//     });
-//   }
-// }
 
-export class Homepage implements OnInit {
-  days = 21;
-  hours = 24;
-  minutes = 60;
-  seconds = 60;
+export class HomePage implements AfterViewInit {
 
-  targetDate = new Date('2026-04-16T08:15:05');
-
-  constructor(private cd: ChangeDetectorRef) {}
-
-  ngOnInit() {
-    setInterval(() => {
-      const now = new Date().getTime();
-      const distance = this.targetDate.getTime() - now;
-
-      if (distance < 0) return;
-
-      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      this.hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
-      this.minutes = Math.floor((distance / (1000 * 60)) % 60);
-      this.seconds = Math.floor((distance / 1000) % 60);
-
-      console.log(this.seconds); // debug
-
-    }, 1000);
+  ngAfterViewInit(): void {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.4
+    });
   }
-
-  // startCountdown() {
-  //   setInterval(() => {
-  //     const now = new Date().getTime();
-  //     const distance = this.targetDate.getTime() - now;
-  //
-  //     if (distance < 0) return;
-  //
-  //     this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  //     this.hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
-  //     this.minutes = Math.floor((distance / (1000 * 60)) % 60);
-  //     this.seconds = Math.floor((distance / 1000) % 60);
-  //
-  //     this.cd.detectChanges(); // 🔥 force update
-  //
-  //   }, 1000);
-
 }
